bae6a77a19f03b2271d3037cc0a31d5413a5ab9a
Bingdi Zhou <bzhou26@my.bcit.ca>
2024-03-07 19:53:45 -0800

    Api URL

--- students-react-ts/src/config/index.ts
+++ students-react-ts/src/config/index.ts
@@ -1,6 +1,7 @@
 const Config = {
-    API_BASE_URL: `/api/`,
-  };
-  
-  export default Config;
+  API_BASE_URL: `https://functions4870mze.azurewebsites.net/api/`,
+};
+
+export default Config;
+
   
